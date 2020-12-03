2b2ae147992fdd799c41961716c983c1f761c029
tomicnikola <50-2018@ftn.edu.rs>
2020-12-03 16:40:49 +0100

    Kreirana Web aplikacija

---
+++ PresentationLayer/PresentationLayerWeb/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PresentationLayerWeb
+{
+    public partial class _Default : Page
+    {
+        private VetBusiness vetBusiness;
+     
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.vetBusiness = new VetBusiness();
+            RefreshData();
+        }
+        public void RefreshData()
+        {
+            List<Vet> v = this.vetBusiness.GetAllVets();
+            listBoxVets.Items.Clear();
+
+            foreach (Vet vets in v)
+                listBoxVets.Items.Add(vets.Id + ". " + vets.FullName + "- " + vets.Specialty + "- " + vets.YearsExperience);
+        }
+    }
+}
